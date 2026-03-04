bf970b66fea8c63b94b970b8b742c48efe12afcb
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-03-04 22:50:20 +0530

    Add mapToProductResponse method to convert Product to ProductResponse

--- src/main/java/edu/icet/service/ProductService.java
+++ src/main/java/edu/icet/service/ProductService.java
@@ -35,9 +35,17 @@ public class ProductService {
     public List<ProductResponse> getAllProducts() {
 
         List<Product> products = productRepository.findAll();
-
         return products.stream().map(this.mapToProductResponse).toList()
 
+    }
+
+    private ProductResponse mapToProductResponse(Product product){
+        return  ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
     }
 
 
