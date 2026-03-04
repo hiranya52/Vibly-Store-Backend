1c75fd2a02369fa9daaa2d19e0f3bdd0dba8745c
Hiranya Mendis <hiranyamendis52@gmail.com>
2026-03-04 22:19:38 +0530

    Call ProductService#createProduct in ProductController

--- src/main/java/edu/icet/controller/ProductController.java
+++ src/main/java/edu/icet/controller/ProductController.java
@@ -1,17 +1,22 @@
 package edu.icet.controller;
 
 import edu.icet.model.dto.ProductDTO;
+import edu.icet.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductDTO productDTO){
-
+        productService.createProduct(productDTO);
     }
 
 }
---
+++ src/main/java/edu/icet/service/ProductService.java
@@ -0,0 +1,18 @@
+package edu.icet.service;
+
+import edu.icet.model.entity.Product;
+import edu.icet.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+
+
+}
